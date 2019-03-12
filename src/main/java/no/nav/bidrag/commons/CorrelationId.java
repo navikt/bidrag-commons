29e3c66248e8f5de0468d53075085959c1fafab0
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-03-12 15:30:31 +0100

    CorrelationId as a value bean

---
+++ src/main/java/no/nav/bidrag/commons/CorrelationId.java
@@ -0,0 +1,33 @@
+package no.nav.bidrag.commons;
+
+public class CorrelationId {
+
+  private static final ThreadLocal<String> CORRELATION_ID_VALUE = new ThreadLocal<>();
+
+  private final String idValue;
+
+  public CorrelationId(String correlationId) {
+    idValue = correlationId;
+    CORRELATION_ID_VALUE.set(idValue);
+  }
+
+  public CorrelationId(IdValue idValue) {
+    String currentTimeAsString = Long.toHexString(System.currentTimeMillis());
+    this.idValue = currentTimeAsString + '(' + idValue.get() + ')';
+    CORRELATION_ID_VALUE.set(this.idValue);
+  }
+
+  public String get() {
+    return idValue;
+  }
+
+  public static String fetchCorrelationIdForThread() {
+    return CORRELATION_ID_VALUE.get();
+  }
+
+  @FunctionalInterface
+  public interface IdValue {
+
+    String get();
+  }
+}
