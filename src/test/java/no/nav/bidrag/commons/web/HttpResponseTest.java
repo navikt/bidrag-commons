8ac3704d119bfccc637157740ddd9625b678c6c4
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-09-24 17:09:15 +0200

    HttpResponse: ny metode clearContentHeaders()

--- src/test/java/no/nav/bidrag/commons/web/HttpResponseTest.java
+++ src/test/java/no/nav/bidrag/commons/web/HttpResponseTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 class HttpResponseTest {
 
@@ -25,6 +26,31 @@ class HttpResponseTest {
         () -> assertThat(responseEntity.getBody()).as("body").isEqualTo(101),
         () -> assertThat(responseEntity.getHeaders()).as("headers").isNotNull(),
         () -> assertThat(responseEntity.getHeaders().getFirst("SVADA")).as("header").isEqualTo("lada"),
+        () -> assertThat(responseEntity.getStatusCode()).as("status").isEqualTo(HttpStatus.I_AM_A_TEAPOT)
+    );
+  }
+
+  @Test
+  @DisplayName("skal ikke videresende Contenth* headers")
+  void skalIkkeVideresendeHeaderContentLength() {
+    var httpHeaders = new HttpHeaders();
+    httpHeaders.add(HttpHeaders.CONTENT_DISPOSITION, "disp...");
+    httpHeaders.add(HttpHeaders.CONTENT_ENCODING, "encode...");
+    httpHeaders.add(HttpHeaders.CONTENT_LANGUAGE, "no...");
+    httpHeaders.add(HttpHeaders.CONTENT_LENGTH, "så mye...");
+    httpHeaders.add(HttpHeaders.CONTENT_LOCATION, "på nett...");
+    httpHeaders.add(HttpHeaders.CONTENT_RANGE, "hit og dit...");
+    httpHeaders.add(HttpHeaders.CONTENT_TYPE, "svada...");
+    httpHeaders.add(HttpHeaders.WARNING, "high tide");
+
+    var responseEntity = new HttpResponse<Object>(new ResponseEntity<>(101, httpHeaders, HttpStatus.I_AM_A_TEAPOT))
+        .clearContentHeaders()
+        .getResponseEntity();
+
+    assertAll(
+        () -> assertThat(responseEntity.getBody()).as("body").isEqualTo(101),
+        () -> assertThat(responseEntity.getHeaders()).as("headers").isNotNull().hasSize(1),
+        () -> assertThat(responseEntity.getHeaders().getFirst(HttpHeaders.WARNING)).as("header").isEqualTo("high tide"),
         () -> assertThat(responseEntity.getStatusCode()).as("status").isEqualTo(HttpStatus.I_AM_A_TEAPOT)
     );
   }
