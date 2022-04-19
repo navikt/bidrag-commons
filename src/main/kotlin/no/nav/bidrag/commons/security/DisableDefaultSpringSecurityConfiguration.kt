86b6a28c1bd562fbbb0cea0db808103f8e327497
Ugur Cenar <cenar93@gmail.com>
2022-04-19 11:50:32 +0200

    Feilretting av skru av default spring configurasjon

--- src/main/kotlin/no/nav/bidrag/commons/security/DisableDefaultSpringSecurityConfiguration.kt
+++ src/main/kotlin/no/nav/bidrag/commons/security/DisableDefaultSpringSecurityConfiguration.kt
@@ -1,8 +1,12 @@
 package no.nav.bidrag.commons.security
 
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
+@ConditionalOnDefaultWebSecurity
+@EnableWebSecurity
 class DisableDefaultSpringSecurityConfiguration: WebSecurityConfigurerAdapter() {
 
     override fun configure(web: WebSecurity) {
