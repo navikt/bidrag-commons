66cd89b99a6e16ccb0907ccc29245831479b5053
Ugur Cenar <cenar93@gmail.com>
2022-06-21 07:50:17 +0200

    Oppdatert CorsFilter

---
+++ src/main/java/no/nav/bidrag/commons/web/DefaultCorsFilter.java
@@ -0,0 +1,24 @@
+package no.nav.bidrag.commons.web;
+
+import java.util.List;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+public class DefaultCorsFilter extends CorsFilter {
+
+  public static UrlBasedCorsConfigurationSource getDefaultCorsConfiguration(){
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowedOrigins(List.of("*"));
+    corsConfiguration.setAllowedHeaders(List.of("Content-Type", "Authorization", "Content-Length", "X-Requested-With", "X-Correlation-ID", "X-Enhet", "Nav-Call-Id", "Nav-Consumer-Id"));
+    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+    UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+    urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+    return urlBasedCorsConfigurationSource;
+  }
+
+  public DefaultCorsFilter() {
+    super(DefaultCorsFilter.getDefaultCorsConfiguration());
+  }
+
+}
