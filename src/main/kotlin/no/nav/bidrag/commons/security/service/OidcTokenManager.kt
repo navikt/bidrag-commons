b04f3f1dbd0d57d0c51c0bcd967c716e12c1a53f
Bent J. Lorentzen <bent.lorentzen@nav.no>
2022-11-22 10:59:07 +0100

    Byttet tokenValidationContextHolder med SpringTokenValidationContextHolder for å minske konfigurasjonsbehovet ved oppstart.

--- src/main/kotlin/no/nav/bidrag/commons/security/service/OidcTokenManager.kt
+++ src/main/kotlin/no/nav/bidrag/commons/security/service/OidcTokenManager.kt
@@ -1,35 +1,34 @@
 package no.nav.bidrag.commons.security.service
 
-import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import no.nav.security.token.support.core.jwt.JwtToken
-import java.util.Optional
+import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 
-open class OidcTokenManager(private val tokenValidationContextHolder: TokenValidationContextHolder) {
+class OidcTokenManager {
     companion object {
         const val AZURE_ISSUER = "aad"
         const val STS_ISSUER = "sts"
     }
-    open fun fetchTokenAsString(): String {
+    fun fetchTokenAsString(): String {
         return fetchToken().tokenAsString
     }
 
     private fun hasIssuers(): Boolean {
-        return tokenValidationContextHolder.tokenValidationContext.issuers.isNotEmpty()
+        return SpringTokenValidationContextHolder().tokenValidationContext.issuers.isNotEmpty()
     }
 
-    open fun isValidTokenIssuedByAzure(): Boolean {
-        return hasIssuers() && tokenValidationContextHolder.tokenValidationContext.getJwtToken(AZURE_ISSUER) != null
+    fun isValidTokenIssuedByAzure(): Boolean {
+        return hasIssuers() && SpringTokenValidationContextHolder().tokenValidationContext.getJwtToken(AZURE_ISSUER) != null
     }
 
-    open fun isValidTokenIssuedBySTS(): Boolean {
-        return hasIssuers() && tokenValidationContextHolder.tokenValidationContext.getJwtToken(STS_ISSUER) != null
+    fun isValidTokenIssuedBySTS(): Boolean {
+        return hasIssuers() && SpringTokenValidationContextHolder().tokenValidationContext.getJwtToken(STS_ISSUER) != null
     }
 
-    open fun getIssuer(): String {
+    fun getIssuer(): String {
         return fetchToken().issuer
     }
 
-    open fun fetchToken(): JwtToken {
-        return tokenValidationContextHolder.tokenValidationContext?.firstValidToken?.orElse(null) ?: throw IllegalStateException("Fant ingen gyldig token i kontekst")
+    fun fetchToken(): JwtToken {
+        return SpringTokenValidationContextHolder().tokenValidationContext.firstValidToken?.orElse(null) ?: throw IllegalStateException("Fant ingen gyldig token i kontekst")
     }
 }
