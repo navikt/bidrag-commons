631e859aa3d67f4e2689d09f03edfd3b830d86db
Ugur Cenar <cenar93@gmail.com>
2022-11-24 13:57:01 +0100

    Create securityTokenServiceSTS used when STS token is used

--- src/main/kotlin/no/nav/bidrag/commons/security/SecurityConfig.kt
+++ src/main/kotlin/no/nav/bidrag/commons/security/SecurityConfig.kt
@@ -3,7 +3,6 @@ package no.nav.bidrag.commons.security
 import no.nav.bidrag.commons.security.service.AzureTokenService
 import no.nav.bidrag.commons.security.service.OidcTokenManager
 import no.nav.bidrag.commons.security.service.SecurityTokenService
-import no.nav.bidrag.commons.security.service.StsTokenService
 import no.nav.bidrag.commons.security.service.TokenService
 import no.nav.bidrag.commons.security.service.TokenXTokenService
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
@@ -11,13 +10,8 @@ import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 
 @Configuration
 @EnableJwtTokenValidation
