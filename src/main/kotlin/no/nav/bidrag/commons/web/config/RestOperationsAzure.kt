d198402df3857bcfb00cba686a5bfb81fbcb6718
Bent J. Lorentzen <bent.lorentzen@nav.no>
2022-12-01 09:57:08 +0100

    Lagt til restOperations, som kun bruker serviceUser for autorisasjon.

--- src/main/kotlin/no/nav/bidrag/commons/web/config/RestOperationsAzure.kt
+++ src/main/kotlin/no/nav/bidrag/commons/web/config/RestOperationsAzure.kt
@@ -1,6 +1,7 @@
 package no.nav.bidrag.commons.web.config
 
 import no.nav.bidrag.commons.web.interceptor.BearerTokenClientInterceptor
+import no.nav.bidrag.commons.web.interceptor.ServiceUserAuthTokenInterceptor
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,12 +9,18 @@ import org.springframework.context.annotation.Import
 
 @Suppress("SpringFacetCodeInspection")
 @Configuration
-@Import(RestTemplateBuilderBean::class, BearerTokenClientInterceptor::class)
+@Import(RestTemplateBuilderBean::class, BearerTokenClientInterceptor::class, ServiceUserAuthTokenInterceptor::class)
 class RestOperationsAzure {
 
   @Bean("azure")
   fun restOperationsJwtBearer(
     restTemplateBuilder: RestTemplateBuilder,
     bearerTokenClientInterceptor: BearerTokenClientInterceptor
+  ) = restTemplateBuilder.additionalInterceptors(bearerTokenClientInterceptor).build()
+
+  @Bean("azureService")
+  fun restOperationsServiceJwtBearer(
+    restTemplateBuilder: RestTemplateBuilder,
+    bearerTokenClientInterceptor: ServiceUserAuthTokenInterceptor
   ) = restTemplateBuilder.additionalInterceptors(bearerTokenClientInterceptor).build()
 }
