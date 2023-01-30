845eece0950986a578ad9834afad26f50140f448
Ugur Cenar <cenar93@gmail.com>
2023-01-30 12:48:12 +0100

    Lagt til options i `AbstractRestClient`

--- src/main/kotlin/no/nav/bidrag/commons/security/SikkerhetsKontekst.kt
+++ src/main/kotlin/no/nav/bidrag/commons/security/SikkerhetsKontekst.kt
@@ -1,19 +1,19 @@
 package no.nav.bidrag.commons.security
 
 object SikkerhetsKontekst {
-  private val ER_I_APPLIKASJON_KONTEKST = ThreadLocal<Boolean>()
+  private val ER_I_APPLIKASJONSKONTEKST = ThreadLocal<Boolean>()
 
   fun <R> medApplikasjonKontekst(func: () -> R): R {
-    ER_I_APPLIKASJON_KONTEKST.set(true)
+    ER_I_APPLIKASJONSKONTEKST.set(true)
     return try {
       func.invoke()
     } finally {
-      ER_I_APPLIKASJON_KONTEKST.set(false)
+      ER_I_APPLIKASJONSKONTEKST.set(false)
     }
   }
 
   fun erIApplikasjonKontekst(): Boolean {
-    return ER_I_APPLIKASJON_KONTEKST.get() ?: false
+    return ER_I_APPLIKASJONSKONTEKST.get() ?: false
   }
 
 }
