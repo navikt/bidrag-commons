4120d677753d3522844942ccec2ae7446a641c8a
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2023-04-25 15:54:35 +0200

    Fjernet NaisProxyCustomizer.kt (#224)

--- src/main/kotlin/no/nav/bidrag/commons/web/config/RestTemplateBuilderBean.kt
+++ src/main/kotlin/no/nav/bidrag/commons/web/config/RestTemplateBuilderBean.kt
@@ -2,7 +2,6 @@ package no.nav.bidrag.commons.web.config
 
 import no.nav.bidrag.commons.web.interceptor.ConsumerIdClientInterceptor
 import no.nav.bidrag.commons.web.interceptor.MdcValuesPropagatingClientInterceptor
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,31 +11,10 @@ import java.time.temporal.ChronoUnit
 
 @Suppress("SpringFacetCodeInspection")
 @Configuration
-@Import(ConsumerIdClientInterceptor::class, NaisProxyCustomizer::class)
+@Import(ConsumerIdClientInterceptor::class)
 class RestTemplateBuilderBean {
 
     @Bean
-    @ConditionalOnProperty("no.nav.security.jwt.issuer.aad.proxy_url")
-    fun restTemplateBuilder(
-        iNaisProxyCustomizer: INaisProxyCustomizer,
-        consumerIdClientInterceptor: ConsumerIdClientInterceptor
-    ) = RestTemplateBuilder()
-        .additionalInterceptors(consumerIdClientInterceptor, MdcValuesPropagatingClientInterceptor())
-        .additionalCustomizers(iNaisProxyCustomizer)
-        .setConnectTimeout(Duration.of(15, ChronoUnit.SECONDS))
-        .setReadTimeout(Duration.of(30, ChronoUnit.SECONDS))
-
-    /**
-     * Denne bønnnen initialiseres hvis proxy-url ikke finnes. Hvis proxy-url finnnes vil bønnen over
-     * initialiseres og denne initialiseres ikke med mindre proxyen har verdien "umulig verdi",
-     * som den aldri skal ha.
-     */
-    @Bean
-    @ConditionalOnProperty(
-        "no.nav.security.jwt.issuer.aad.proxy_url",
-        matchIfMissing = true,
-        havingValue = "Umulig verdi"
-    )
     fun restTemplateBuilderNoProxy(
         consumerIdClientInterceptor: ConsumerIdClientInterceptor
     ): RestTemplateBuilder = RestTemplateBuilder()
