f05b7ef302239b786f26df4530f6932479dc5f40
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2023-05-03 09:38:45 +0200

    Lagt til ObservationRestTemplateCustomizer (#228)

--- src/main/kotlin/no/nav/bidrag/commons/web/config/RestTemplateBuilderBean.kt
+++ src/main/kotlin/no/nav/bidrag/commons/web/config/RestTemplateBuilderBean.kt
@@ -1,11 +1,14 @@
 package no.nav.bidrag.commons.web.config
 
+import io.micrometer.observation.ObservationRegistry
 import no.nav.bidrag.commons.web.interceptor.ConsumerIdClientInterceptor
 import no.nav.bidrag.commons.web.interceptor.MdcValuesPropagatingClientInterceptor
+import org.springframework.boot.actuate.metrics.web.client.ObservationRestTemplateCustomizer
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import org.springframework.http.client.observation.DefaultClientRequestObservationConvention
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 
@@ -16,9 +19,16 @@ class RestTemplateBuilderBean {
 
     @Bean
     fun restTemplateBuilderNoProxy(
-        consumerIdClientInterceptor: ConsumerIdClientInterceptor
+        consumerIdClientInterceptor: ConsumerIdClientInterceptor,
+        observationRestTemplateCustomizer: ObservationRestTemplateCustomizer
     ): RestTemplateBuilder = RestTemplateBuilder()
         .additionalInterceptors(consumerIdClientInterceptor, MdcValuesPropagatingClientInterceptor())
+        .additionalCustomizers(observationRestTemplateCustomizer)
         .setConnectTimeout(Duration.of(15, ChronoUnit.SECONDS))
         .setReadTimeout(Duration.of(30, ChronoUnit.SECONDS))
+
+    @Bean
+    fun metricsRestTemplateCustomizer(): ObservationRestTemplateCustomizer {
+        return ObservationRestTemplateCustomizer(ObservationRegistry.create(), DefaultClientRequestObservationConvention())
+    }
 }
