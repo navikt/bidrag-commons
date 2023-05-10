8b93e85a117afcaa1d2c9469473c0daa9934939a
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2023-05-10 13:07:46 +0200

    Endret type på inputparameter parameter til interface. (#229)

--- src/main/kotlin/no/nav/bidrag/commons/web/HttpResponse.kt
+++ src/main/kotlin/no/nav/bidrag/commons/web/HttpResponse.kt
@@ -1,7 +1,7 @@
 package no.nav.bidrag.commons.web
 
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.util.CollectionUtils
 import java.util.*
@@ -33,27 +33,27 @@ class HttpResponse<T>(val responseEntity: ResponseEntity<T>) {
 
         val headers = HttpHeaders(CollectionUtils.toMultiValueMap(headersMap))
         headers.clearContentHeaders()
-        return from(responseEntity.body, headers, HttpStatus.valueOf(responseEntity.statusCode.value()))
+        return from(responseEntity.body, headers, responseEntity.statusCode)
     }
 
     companion object {
-        fun <E> from(httpStatus: HttpStatus?): HttpResponse<E> {
-            val responseEntity = ResponseEntity<E>(httpStatus!!)
+        fun <E> from(httpStatus: HttpStatusCode): HttpResponse<E> {
+            val responseEntity = ResponseEntity<E>(httpStatus)
             return HttpResponse(responseEntity)
         }
 
-        fun <E> from(httpStatus: HttpStatus?, body: E): HttpResponse<E> {
-            val responseEntity = ResponseEntity(body, httpStatus!!)
+        fun <E> from(httpStatus: HttpStatusCode, body: E): HttpResponse<E> {
+            val responseEntity = ResponseEntity(body, httpStatus)
             return HttpResponse(responseEntity)
         }
 
-        fun <E> from(httpHeaders: HttpHeaders?, httpStatus: HttpStatus?): HttpResponse<E> {
-            val responseEntity = ResponseEntity<E>(httpHeaders!!, httpStatus!!)
+        fun <E> from(httpHeaders: HttpHeaders, httpStatus: HttpStatusCode): HttpResponse<E> {
+            val responseEntity = ResponseEntity<E>(httpHeaders, httpStatus)
             return HttpResponse(responseEntity)
         }
 
-        fun <E> from(body: E, httpHeaders: HttpHeaders?, httpStatus: HttpStatus?): HttpResponse<E> {
-            val responseEntity = ResponseEntity(body, httpHeaders, httpStatus!!)
+        fun <E> from(body: E, httpHeaders: HttpHeaders?, httpStatus: HttpStatusCode): HttpResponse<E> {
+            val responseEntity = ResponseEntity(body, httpHeaders, httpStatus)
             return HttpResponse(responseEntity)
         }
     }
