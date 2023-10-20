5015a9f7bd5aeaa736949217b5495409988bba1c
Simen Høston <simen.naper.hoston@webstep.no>
2023-10-20 08:54:30 +0200

    Legger til tester av maskinporten token cache og token generator

---
+++ src/test/kotlin/no/nav/bidrag/commons/security/maskinporten/MaskinportenTestUtils.kt
@@ -0,0 +1,21 @@
+package no.nav.bidrag.commons.security.maskinporten
+
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.JWSHeader
+import com.nimbusds.jose.crypto.RSASSASigner
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator
+import com.nimbusds.jwt.JWTClaimsSet
+import com.nimbusds.jwt.SignedJWT
+import java.util.Date
+
+object MaskinportenTestUtils {
+
+    fun opprettMaskinportenToken(utgarOm: Int): String {
+        val privateKey = RSAKeyGenerator(2048).keyID("123").generate()
+
+        val jwtClaimsSet = JWTClaimsSet.Builder().expirationTime(Date(Date().time + (utgarOm * 1000))).build()
+        val signedJwt = SignedJWT(JWSHeader.Builder(JWSAlgorithm.RS256).keyID(privateKey.keyID).build(), jwtClaimsSet)
+        signedJwt.sign(RSASSASigner(privateKey))
+        return signedJwt.serialize()
+    }
+}
