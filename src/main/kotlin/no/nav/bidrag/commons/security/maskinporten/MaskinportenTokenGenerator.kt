f9290555a774b573d44d9e801d1e4d6b17e2d83f
simhos <104764765+simhos@users.noreply.github.com>
2023-10-20 08:15:12 +0200

    Legger til maskinporten klient for uthenting av JWT token fra maskinporten (#253)

---
+++ src/main/kotlin/no/nav/bidrag/commons/security/maskinporten/MaskinportenTokenGenerator.kt
@@ -0,0 +1,34 @@
+package no.nav.bidrag.commons.security.maskinporten
+
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.JWSHeader
+import com.nimbusds.jose.crypto.RSASSASigner
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jwt.JWTClaimsSet
+import com.nimbusds.jwt.SignedJWT
+import java.util.Date
+
+class MaskinportenTokenGenerator(
+    val maskinportenConfig: MaskinportenConfig
+) {
+
+    internal fun genererJwtToken(scope: String): String {
+        return SignedJWT(opprettJwsHeader(), generateJWTClaimSet(scope)).apply {
+            sign(RSASSASigner(opprettRsaKey()))
+        }.serialize()
+    }
+
+    private fun generateJWTClaimSet(scope: String): JWTClaimsSet {
+        return JWTClaimsSet.Builder().apply {
+            audience(maskinportenConfig.audience)
+            issuer(maskinportenConfig.clientId)
+            claim("scope", scope)
+            issueTime(Date(Date().time))
+            expirationTime(Date(Date().time + (maskinportenConfig.validInSeconds * 1000)))
+        }.build()
+    }
+
+    private fun opprettJwsHeader(): JWSHeader = JWSHeader.Builder(JWSAlgorithm.RS256).keyID(opprettRsaKey().keyID).build()
+
+    private fun opprettRsaKey(): RSAKey = RSAKey.parse(maskinportenConfig.privateKey)
+}
---
+++ src/main/kotlin/no/nav/bidrag/commons/security/maskinporten/MaskinportenTokenResponse.kt
@@ -0,0 +1,8 @@
+package no.nav.bidrag.commons.security.maskinporten
+
+data class MaskinportenTokenResponse(
+    val access_token: String,
+    val token_type: String?,
+    val expires_in: Int?,
+    val scope: String?
+)
