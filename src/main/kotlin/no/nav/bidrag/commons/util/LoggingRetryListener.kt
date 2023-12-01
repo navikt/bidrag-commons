b1af3cefda6c0d5594582bc0aded9a231494acba
Ugur Cenar <cenar93@gmail.com>
2023-12-01 13:09:22 +0100

    SaksbehandlernavnProvider med retry logikk

---
+++ src/main/kotlin/no/nav/bidrag/commons/util/LoggingRetryListener.kt
@@ -0,0 +1,15 @@
+package no.nav.bidrag.commons.util
+
+import org.slf4j.LoggerFactory
+import org.springframework.retry.RetryCallback
+import org.springframework.retry.RetryContext
+import org.springframework.retry.RetryListener
+private val log = LoggerFactory.getLogger(LoggingRetryListener::class.java)
+
+class LoggingRetryListener(val details: String? = null) : RetryListener {
+    override fun <T : Any?, E : Throwable?> onError(context: RetryContext?, callback: RetryCallback<T, E>?, throwable: Throwable?) {
+        super.onError(context, callback, throwable)
+        val details = if (!details.isNullOrEmpty()) " med detaljer: $details" else ""
+        log.warn("Det skjedde en feil i retryTemplate$details. Dette er ${context?.retryCount}. forsøk.", throwable)
+    }
+}
